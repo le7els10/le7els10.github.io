fd29249ded894ac0aaf722fe9d82a34472e25d7b
Juan Nuñez <le7els10@gmail.com>
2023-07-27 17:27:56 -0500

    add tablet

--- logic.js
+++ logic.js
@@ -10,10 +10,25 @@ const detectSo = () => {
   return "ios";
 };
 
+const deviceType = () => {
+  const ua = navigator.userAgent;
+  if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+    return "tablet";
+  } else if (
+    /Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
+      ua
+    )
+  ) {
+    return "mobile";
+  }
+  return "desktop";
+};
+
 let name = detectSo();
 let windowWidth = window.screen.availWidth;
 let windowHeight = window.screen.availHeight;
+let deviceTypeVar = deviceType();
 
 document.getElementById(
   "deviceId"
-).innerText = `${name}, ${windowWidth}x${windowHeight}`;
+).innerText = `${name},${deviceTypeVar}, ${windowWidth}x${windowHeight}`;
