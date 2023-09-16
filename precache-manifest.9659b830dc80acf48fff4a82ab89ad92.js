1dad40312f24acb68fa7c4c78205e915bf3c4963
Juan Nuñez <le7els10@gmail.com>
2023-09-16 12:34:56 -0500

    feature(cv): change deploy

---
+++ precache-manifest.9659b830dc80acf48fff4a82ab89ad92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ead9fc8cb0fa4c0338d01d5f0bcbb5ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69a8b320360c4e90f5cd",
+    "url": "/static/css/main.9a1718dd.chunk.css"
+  },
+  {
+    "revision": "dbed5290fdc7ef8e2d5f",
+    "url": "/static/js/2.785e48fc.chunk.js"
+  },
+  {
+    "revision": "64f54ce285484345cd5e3bd96b120148",
+    "url": "/static/js/2.785e48fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69a8b320360c4e90f5cd",
+    "url": "/static/js/main.ae1e212b.chunk.js"
+  },
+  {
+    "revision": "c4957e584f3aeb929d78",
+    "url": "/static/js/runtime-main.4b6cd884.js"
+  },
+  {
+    "revision": "20fb69cfeaa05e30a0423fe454dcf4f4",
+    "url": "/static/media/face.20fb69cf.jpeg"
+  }
+]);
